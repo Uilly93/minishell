e11a1aa51d805e9a952bbf8516c2840dae35833b
wnocchi <wnocchi@student.42.fr>
2024-06-06 11:51:02 +0200

    cd fixed

--- src/cd.c
+++ src/cd.c
@@ -6,26 +6,32 @@
 /*   By: wnocchi <wnocchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 15:16:05 by wnocchi           #+#    #+#             */
-/*   Updated: 2024/06/05 17:05:20 by wnocchi          ###   ########.fr       */
+/*   Updated: 2024/06/06 11:44:25 by wnocchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-char	*join_path(char *pwd, char *arg)
-{
-	char	*res;
-	char	*buffer;
+// char	*join_path(char *pwd, char *arg)
+// {
+// 	char	*res;
+// 	char	*buffer;
 
-	buffer = join_free(pwd, "/");
-	if (!buffer)
-		return (NULL);
-	res = join_free(buffer, arg);
-	if (!res)
-		return (NULL);
-	return (res);
+// 	buffer = join_free(pwd, "/");
+// 	if (!buffer)
+// 		return (NULL);
+// 	res = join_free(buffer, arg);
+// 	if (!res)
+// 		return (NULL);
+// 	return (res);
+// }
+void ft_err(char *error)
+{
+	write(2, error, ft_strlen(error));
+	write(2, "\n", 1);
 }
 
 int	ft_cd(char **arg)
@@ -33,29 +39,28 @@ int	ft_cd(char **arg)
 	char	*pwd;
 	char	*path;
 
-	if(*arg && ft_strcmp(*arg, "cd") == 0)
+	if(*arg && ft_strcmp(*arg++, "cd") == 0)
 	{
 		pwd = getcwd(NULL, 0);
 		if (!pwd)
 			return (1);
-		arg++;
 		if(*arg == NULL)
-		{
 			path = ft_strdup("/"); // need env to fix
-			free(pwd);
-		}
 		else
 		{
-			path = ft_strjoin(pwd, *arg);
-			free(pwd);
+			path = ft_strjoin(pwd, "/");
+			path = join_free(path, *arg++);
 		}
+		free(pwd);
 		if (!path)
 			return (1);
-		// if(++*arg != NULL)
-		// 	return (write(2, "minishell: cd: too many arguments\n", 34), 1);
+		if(*arg)
+			return (ft_err("minishell: cd: too many arguments"), free(path), 1);
 		if (chdir(path) == -1)
-			return (perror("minishell: cd "), free(path), 1);
+			return (perror("minishell: cd"), free(path), 1);
 		free(path);
 	}
 	return (0);
 }
+
+// write(2, "msh: cd:too many arguments\n", 27)
