5314a729229c720e5decacc5144d0ff843001398
wnocchi <wnocchi@student.42.fr>
2024-07-26 09:00:12 +0200

    ft_echo shorten

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: wnocchi <wnocchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 09:01:56 by wnocchi           #+#    #+#             */
-/*   Updated: 2024/07/25 13:53:50 by wnocchi          ###   ########.fr       */
+/*   Updated: 2024/07/26 08:58:41 by wnocchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,11 @@ int	which_fd(t_msh *msh)
 		return (1);
 }
 
-static int	handle_flag(char **prompt, int i, int j)
+static int	handle_flag(char **prompt, int i)
 {
+	int j;
+
+	j = 1;
 	while (prompt[i][j])
 	{
 		if (prompt[i][j] != 'n')
@@ -40,10 +43,12 @@ static int	handle_flag(char **prompt, int i, int j)
 	return (1);
 }
 
-void	print_args(t_msh *msh, int j, int i, bool new_line)
+void	print_args(t_msh *msh, int i, bool new_line)
 {
 	const int	fd = which_fd(msh);
+	int j;
 
+	j = 1;
 	if (fd == -1)
 	{
 		perror("msh");
@@ -66,21 +71,19 @@ void	print_args(t_msh *msh, int j, int i, bool new_line)
 void	ft_echo(t_msh *msh)
 {
 	int		i;
-	int		j;
 	bool	new_line;
 
 	i = 0;
-	j = 1;
 	new_line = true;
 	if (msh->cmd[0] && ft_strcmp(msh->cmd[i++], "echo") == 0)
 	{
 		if (msh->cmd[i] == NULL)
-			return ;
+			return (ft_printf(which_fd(msh), "\n"), (void)NULL);
 		if (msh->cmd[i][0] != '\0' && ft_strncmp(msh->cmd[i], "-n", 2) == 0)
 		{
 			while (ft_strncmp(msh->cmd[i], "-n", 2) == 0)
 			{
-				if (handle_flag(msh->cmd, i, j) == 0)
+				if (handle_flag(msh->cmd, i) == 0)
 					break ;
 				new_line = false;
 				i++;
@@ -88,6 +91,6 @@ void	ft_echo(t_msh *msh)
 					return ;
 			}
 		}
-		print_args(msh, j, i, new_line);
+		print_args(msh, i, new_line);
 	}
 }
