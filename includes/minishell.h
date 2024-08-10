0a284e3de90269f819e0c80aea4d457bc9f1aa74
Uilly93 <williamnocchi92@gmail.com>
2024-08-10 13:31:00 +0200

    norminette + func splited done

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: wnocchi <wnocchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 09:48:36 by wnocchi           #+#    #+#             */
-/*   Updated: 2024/08/10 13:15:14 by wnocchi          ###   ########.fr       */
+/*   Updated: 2024/08/10 13:29:00 by wnocchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,16 @@ typedef struct s_msh
 
 extern int	g_last_sig;
 
+/*BUILTINS*/
+int		ft_env(t_env **env, t_msh *msh);
+int		ft_unset(t_env **env, char **av);
+void	ft_echo(t_msh *msh);
+int		ft_cd(char **arg, t_env **env);
+int		ft_exit(t_msh *msh, t_env **env);
+int		get_pwd(char **arg, t_msh *msh);
+int		ft_export(t_msh *msh, t_env **env);
+
+/*EXEC*/
 int		msh_loop(t_msh *msh, t_env **env);
 char	*custom_prompt(t_env **env);
 void	wait_pids(t_env **env);
@@ -74,9 +84,7 @@ int		print_export(char **sorted, t_msh *msh);
 char	**sort_env(char **tab, t_env *env);
 t_env	*create_env_node(char **envp, int i);
 void	set_excode(t_env **env, int code);
-void	add_underscore(t_env **env);
 void	add_env_node(t_env **lst, t_env *add);
-t_env	*no_env(char *full_var);
 void	setup_exec_signals(void);
 int		split_env(t_env **env);
 int		is_equal(char *var);
@@ -84,15 +92,8 @@ char	*get_key_env(char *var);
 char	*get_value_env(char *var);
 int		env_len(t_env *env);
 int		update_env(t_env **env);
-int		ft_env(t_env **env, t_msh *msh);
-int		ft_unset(t_env **env, char **av);
 int		ft_del_node(t_env **head, char *av);
 int		here_doc(t_msh *msh);
-void	ft_echo(t_msh *msh);
-int		ft_cd(char **arg, t_env **env);
-int		ft_exit(t_msh *msh, t_env **env);
-int		get_pwd(char **arg, t_msh *msh);
-int		ft_export(t_msh *msh, t_env **env);
 int		free_env(t_env **env);
 t_env	**env_into_list(char **envp);
 t_msh	*ft_lastnode(t_msh *lst);
@@ -102,15 +103,11 @@ int		close_pipes(t_msh *msh);
 int		close_files(t_msh *msh);
 void	free_tab(char **tab);
 int		get_flags(t_msh *msh);
-int		redirect_fd_write(t_msh *msh, int *pipefd);
 int		ft_lstlen(t_msh *msh);
-void	ft_free(void *pointer);
 char	*join_path_access(char *av, t_env *env);
-char	**get_path(t_env *env);
 void	ft_err(char *error);
 int		init_sigint(void);
 char	**get_env(t_env *env);
-void	signal_handler(int sig, siginfo_t *info, void *context);
 void	free_lst(t_msh *msh);
 
 /*PARSING*/
